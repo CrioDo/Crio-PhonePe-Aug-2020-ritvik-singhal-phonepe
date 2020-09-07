b3660aa5afb10e2908f08b2c775831f25574f3c8
Jenkins <jenkins@common-infra-jenkins-1.asia-south1-c.c.crio-vpn-common.internal>
2020-09-07 10:32:19 +0530

    Updates

---
+++ precache-manifest.13fe61a25a06d8932d9fe0cf228a89e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46455fc955bf068ac268caed499dbaf7",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/index.html"
+  },
+  {
+    "revision": "dbcee2bc1d0c799241b2",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "af76f68d5fc96ab8ff69",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "dbcee2bc1d0c799241b2",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/js/2.6e12d535.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/js/2.6e12d535.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af76f68d5fc96ab8ff69",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/js/main.32e8381d.chunk.js"
+  },
+  {
+    "revision": "bf572f0c70d14cee9a0c",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/js/runtime-main.b31b63ba.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/Crio-PhonePe-Aug-2020-ritvik-singhal-phonepe/static/media/crio-logo.63be71d0.svg"
+  }
+]);
